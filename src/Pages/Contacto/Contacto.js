9ba5e1d45a92d8987de3ab9f7d3b174878c8be0c
Daniel Aranguren <arangurendaniel93@gmail.com>
2023-06-08 18:24:30 -0300

    detalles

--- src/Pages/Contacto/Contacto.js
+++ src/Pages/Contacto/Contacto.js
@@ -1,60 +1,52 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { db } from '../../firebase.config'
+import { addDoc, collection } from 'firebase/firestore'
+
 import './Contacto.css';
 
 const Contacto = () => {
 
-  const handleSubmit = (e) => {
+  const formularioConsulta = useRef()
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(form);
+    const col = collection(db, 'messages')
+    const newMessage = await addDoc(col, datosUsuario);
+    alert("Mensaje enviado exitosamente");
+    formularioConsulta.current.reset();
+    setDatosUsuario({
+      nombre: "",
+      email: "",
+      mensaje: ""
+    })
+    console.log(datosUsuario);
   }
-/*
-  const [datosCliente, setDatosCliente] = useState({
+
+  const [datosUsuario, setDatosUsuario] = useState({
     nombre: "",
     email: "",
     mensaje: ""
   })
 
-  const handleDatosCliente = (e) => {
-    setDatosCliente({
-      ...datosCliente, 
+  const handleDatosUsuario = (e) => {
+    setDatosUsuario({
+      ...datosUsuario, 
       [e.target.name]: e.target.value
     })
   }
-*/
-
-const [form, setForm] = useState( 
-  {buyer: {
-    nombre: "",
-    email: "",
-    telefono: ""
-}})
-
-const handleForm = (e) => {
- setForm({
-  buyer: {
-    ...form.buyer,
-    [e.target.name]: e.target.value
-  }
- })
-}
 
   return (
     <div>
         <img className='fondo-funkos' src={require("../../imagenes/funkos-contacto.jpg")} alt="Fondo de Funkos" />
-        <form className='formulario-contacto' action="" onSubmit={handleSubmit} >
+        <form ref={formularioConsulta} className='formulario-contacto' onSubmit={handleSubmit} >
             <h1>Dejanos un mensaje</h1>
             <p>Nuestro equipo te contactará pronto</p>
-            <input type="text" placeholder='nombre' name='nombre' onChange={handleForm} />
-            <input type="email" placeholder='email' name='email' onChange={handleForm} />
-            <input type="phone" placeholder='telefono' name='telefono' onChange={handleForm} />
-            {/*
             <label htmlFor="">Nombre</label>
-            <input type="text" name="nombre" value={datosCliente.nombre} onChange={handleDatosCliente} />
+            <input type="text" name="nombre" value={datosUsuario.nombre} onChange={handleDatosUsuario} />
             <label htmlFor="">Email</label>
-            <input type="email" name="email" value={datosCliente.email} onChange={handleDatosCliente} />
+            <input type="email" name="email" value={datosUsuario.email} onChange={handleDatosUsuario} />
             <label htmlFor="">Mensaje</label>
-            <textarea name="mensaje" value={datosCliente.mensaje} onChange={handleDatosCliente} cols="30" rows="10"></textarea>
-  */}
+            <textarea name="mensaje" value={datosUsuario.mensaje} onChange={handleDatosUsuario} cols="30" rows="10"></textarea>
             <button className='boton-enviar' href="">Enviar</button>
         </form>
     </div>
